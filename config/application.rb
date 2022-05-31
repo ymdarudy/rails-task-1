279247a47892fec91721dda1d7b5719fc4195dfd
yuji yamada <ymdarudy@gmail.com>
2022-05-31 12:18:30 +0900

    first commit (create app dir & config timezone)

---
+++ config/application.rb
@@ -0,0 +1,13 @@
+require_relative "boot"
+
+require "rails/all"
+
+Bundler.require(*Rails.groups)
+
+module RailsTask1
+  class Application < Rails::Application
+    config.load_defaults 6.0
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
+  end
+end
