bdc79a9f226f2d7ba79c34b321a7f7d7ebddaa90
yuji yamada <ymdarudy@gmail.com>
2022-05-31 18:13:29 +0900

    add header, footer, css

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit update destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(:created_at)
   end
 
   def new
